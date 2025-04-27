2a93ab277a4fa7e4a16f991ef9e735286b310600
joceesagar <joshisagarm2@gmail.com>
2025-04-27 20:16:21 +0545

    implementing authorization logic

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { getUser, getUsers } from "../controllers/user.controller.js";
+import authorize from "../middlewares/auth.middleware.js";
 
 const userRouter = Router();
 
 userRouter.get("/", getUsers)
-userRouter.get("/:id", getUser)
+userRouter.get("/:id", authorize, getUser) //only the login user can see his details
 userRouter.post("/", (req, res) => res.send({ message: 'CREATE new user' }))
 userRouter.put("/:id", (req, res) => res.send({ message: 'UPDATE user details' }))
 userRouter.delete("/:id", (req, res) => res.send({ message: 'DELETE user details' }))
